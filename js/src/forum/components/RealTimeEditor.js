2146d29f8a924ee57cedf169715799fbac7d7440
Marco Colia <info@justoverclock.it>
2021-05-06 18:28:18 +0200

    2nd try

--- js/src/forum/components/RealTimeEditor.js
+++ js/src/forum/components/RealTimeEditor.js
@@ -1,39 +1,40 @@
-import Page from "flarum/components/Page";
-import IndexPage from "flarum/components/IndexPage";
-import listItems from "flarum/helpers/listItems";
+import Page from 'flarum/components/Page';
+import IndexPage from 'flarum/components/IndexPage';
+import listItems from 'flarum/helpers/listItems';
+import CodeMirror from 'codemirror';
 
 /* global m */
 
-export function refresh(){
-  document.getElementById('output').srcdoc = document.getElementById('editor-text').value;
-}
-
 export default class RealTimeEditor extends Page {
-
   view() {
-    return m(".IndexPage", [
+    return m('.IndexPage', [
       IndexPage.prototype.hero(),
-      m('.container', m('.sideNavContainer', [
-        m('nav.IndexPage-nav.sideNav', m('ul', listItems(IndexPage.prototype.sidebarItems().toArray()))),
-        m('.IndexPage-results.sideNavOffset',[
-          m("div", {className:"hometitle"},
-            m("h1", {className:"maintitle"},
-              "Html Editor"
-            )
-          ),
-          m("div", {className:"mainpar"},
-            m("p", {className: "desc"},
-              "This Html editor support HTML/Css code, simply write your code and see the result in real time in the output box"
-            )
-          ),
-          m("div", {className:"main"},
-            [
-              m("textarea", {id:"editor-text",onkeyup:refresh,placeholder:"Type or paste your code here..."}),
-              m("iframe", {id:"output"})
-            ]
-          )
-        ]),
-      ]))
+      m(
+        '.container',
+        m('.sideNavContainer', [
+          m('nav.IndexPage-nav.sideNav', m('ul', listItems(IndexPage.prototype.sidebarItems().toArray()))),
+          m('.IndexPage-results.sideNavOffset', [
+            m('div', { className: 'hometitle' }, m('h1', { className: 'maintitle' }, 'Html Editor')),
+            m(
+              'div',
+              { className: 'mainpar' },
+              m(
+                'p',
+                { className: 'desc' },
+                'This Html editor support HTML/Css code, simply write your code and see the result in real time in the output box'
+              )
+            ),
+            m('div', { className: 'main' }, [
+              m('textarea', { id: 'editor-text', onkeyup: refresh, placeholder: 'Type or paste your code here...' }),
+              m('iframe', { id: 'output' }),
+            ]),
+          ]),
+        ])
+      ),
     ]);
   }
+}
+function refresh() {
+
+  document.getElementById('output').srcdoc = document.getElementById('editor-text').value;
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,22 +1,22 @@
-import { extend } from "flarum/extend";
-import IndexPage from "flarum/components/IndexPage";
-import LinkButton from "flarum/components/LinkButton";
-import RealTimeEditor from "./components/RealTimeEditor";
-import app from "flarum/app";
+import { extend } from 'flarum/extend';
+import IndexPage from 'flarum/components/IndexPage';
+import LinkButton from 'flarum/components/LinkButton';
+import RealTimeEditor from './components/RealTimeEditor';
+import app from 'flarum/app';
 
-app.initializers.add("justoverclock/flarum-ext-realtimecode", () => {});
-  app.routes.RealTimeEditor = {
-    path: "/editor",
-    component: RealTimeEditor,
-  };
-  extend(IndexPage.prototype, "navItems", (navItems) => {
-    navItems.add(
-      "RealTimeEditor",
-      <LinkButton href={app.route("RealTimeEditor")} icon="fas fa-laptop-code">
-        {app.translator.trans("flarum-ext-realtimecode.forum.title")}
-      </LinkButton>,
-      100
-    );
+app.initializers.add('justoverclock/flarum-ext-realtimecode', () => {});
+app.routes.RealTimeEditor = {
+  path: '/editor',
+  component: RealTimeEditor,
+};
+extend(IndexPage.prototype, 'navItems', (navItems) => {
+  navItems.add(
+    'RealTimeEditor',
+    <LinkButton href={app.route('RealTimeEditor')} icon="fas fa-laptop-code">
+      {app.translator.trans('flarum-ext-realtimecode.forum.title')}
+    </LinkButton>,
+    100
+  );
 
-    return navItems;
-  });
+  return navItems;
+});
