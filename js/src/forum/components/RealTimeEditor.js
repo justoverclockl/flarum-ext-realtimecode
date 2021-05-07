85a211812342acb153d481cd89ad804f47ea8307
Marco Colia <info@justoverclock.it>
2021-05-07 10:34:10 +0200

    1st working version

--- js/src/forum/components/RealTimeEditor.js
+++ js/src/forum/components/RealTimeEditor.js
@@ -1,7 +1,6 @@
 import Page from 'flarum/components/Page';
 import IndexPage from 'flarum/components/IndexPage';
 import listItems from 'flarum/helpers/listItems';
-import CodeMirror from 'codemirror';
 
 /* global m */
 
@@ -14,18 +13,19 @@ export default class RealTimeEditor extends Page {
         m('.sideNavContainer', [
           m('nav.IndexPage-nav.sideNav', m('ul', listItems(IndexPage.prototype.sidebarItems().toArray()))),
           m('.IndexPage-results.sideNavOffset', [
-            m('div', { className: 'hometitle' }, m('h1', { className: 'maintitle' }, 'Html Editor')),
+            m('div', { className: 'hometitle' }, m('h1', { className: 'maintitle' }, app.translator.trans('flarum-ext-realtimecode.forum.htmleditor'))),
             m(
               'div',
               { className: 'mainpar' },
               m(
                 'p',
                 { className: 'desc' },
-                'This Html editor support HTML/Css code, simply write your code and see the result in real time in the output box'
+                app.translator.trans('flarum-ext-realtimecode.forum.description')
               )
             ),
             m('div', { className: 'main' }, [
-              m('textarea', { id: 'editor-text', onkeyup: refresh, placeholder: 'Type or paste your code here...' }),
+              m('textarea', { className: 'contedit', id: 'editor-text', onkeyup: refresh, placeholder: 'Type or paste your code here...' }),
+              m('div', { className: 'hometitle' }, m('h1', { className: 'outptitle' }, 'Live Preview Output')),
               m('iframe', { id: 'output' }),
             ]),
           ]),
@@ -35,6 +35,5 @@ export default class RealTimeEditor extends Page {
   }
 }
 function refresh() {
-
   document.getElementById('output').srcdoc = document.getElementById('editor-text').value;
 }
