0de14518036ea09e5c4c34955b0d0d3e13177dca
Marco Colia <info@justoverclock.it>
2021-05-20 10:01:25 +0200

    Removed unused dependencies

--- js/src/forum/components/RealTimeEditor.js
+++ js/src/forum/components/RealTimeEditor.js
@@ -10,18 +10,15 @@ export default class RealTimeEditor extends Page {
   view() {
     return m('.IndexPage', [
       IndexPage.prototype.hero(),
-      m(
-        '.container',
+      m('.container',
         m('.sideNavContainer', [
           m('nav.IndexPage-nav.sideNav', m('ul', listItems(IndexPage.prototype.sidebarItems().toArray()))),
-          m('.IndexPage-results.sideNavOffset', [
-            m(
-              'div',
-              { className: 'hometitle' },
-              m('h1', { className: 'maintitle' }, app.translator.trans('flarum-ext-realtimecode.forum.htmleditor'))
-            ),
-            m('div', { className: 'mainpar' }, m('p', { className: 'desc' }, app.translator.trans('flarum-ext-realtimecode.forum.description'))),
-            m('div', { className: 'main' }, [
+            m('.IndexPage-results.sideNavOffset', [
+             m('div', { className: 'hometitle' },
+              m('h1', { className: 'maintitle' }, app.translator.trans('flarum-ext-realtimecode.forum.htmleditor'))),
+              m('div',{ className: 'mainpar' },
+              m('p',  { className: 'desc' }, app.translator.trans('flarum-ext-realtimecode.forum.description'))),
+              m('div',{ className: 'main' }, [
               m('textarea', {
                 className: 'contedit',
                 id: 'editortext',
@@ -36,13 +33,10 @@ export default class RealTimeEditor extends Page {
                     this.iframe.srcdoc = cm.getValue();
                   }),
               }),
-              m(
-                'div',
-                { className: 'hometitle' },
+              m('div',  { className: 'hometitle' },
                 m('h1', { className: 'outptitle' }, app.translator.trans('flarum-ext-realtimecode.forum.outputtitle'))
               ),
-              m('iframe', {
-                id: 'output',
+              m('iframe', {id: 'output',
                 oncreate: ({ dom }) => {
                   this.iframe = dom;
                 },
